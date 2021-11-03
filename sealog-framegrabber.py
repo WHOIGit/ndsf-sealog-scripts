e6b69fceae43d6e79eeb5d02cb935e1bb15c1199
Ryan Govostes <rgovostes@whoi.edu>
2021-11-03 22:32:21 +0000

    Add imaging control server support for framegrabber
    Passing the server API endpoint to the --imaging-control flag tells the
    script to take the list of framegrabber devices from heartbeat messages sent
    by the imaging control server over a websocket.
    
    To minimize (not eliminate) the potential for race conditions, frames are
    captured immediately but are not submitted until the following heartbeat,
    which gives us the most up-to-date camera name for that framegrabber.

--- sealog-framegrabber.py
+++ sealog-framegrabber.py
@@ -12,12 +12,15 @@ data is updated to reference the files on disk.
 
 import argparse
 import asyncio
+import collections
 import datetime
 import json
 import logging
 import os
+import urllib.parse
 
 import requests
+import socketio
 import websockets
 
 from python_sealog.settings import apiServerURL, eventAuxDataAPIPath, headers, \
@@ -45,39 +48,32 @@ PING = {
 
 ARGS = None
 
+# Incoming Sealog events get added to this queue and processed by a worker
+Event = collections.namedtuple('Event', 'id timestamp frames')
+EVENT_QUEUE = None  # see https://stackoverflow.com/a/55918049/145504
 
-def download_url(url, f):
+# Records the last heartbeat timestamp from the imaging control server
+LAST_HEARTBEAT = None
+
+
+def download_url(url):
+    logger.info('Downloading frame from %s', url)
+    data = bytearray()
     with requests.get(url, stream=True, timeout=ARGS.timeout) as r:
         r.raise_for_status()
-        for chunk in r.iter_content(chunk_size=8192): 
-            f.write(chunk)
+        for chunk in r.iter_content(chunk_size=8192):
+            data += chunk
+    return data
 
 
-async def handle_event(event):
-    ts = datetime.datetime.strptime(event['message']['ts'],
-                                    '%Y-%m-%dT%H:%M:%S.%fZ')
-    if (datetime.datetime.utcnow() - ts).total_seconds() > ARGS.max_age:
-         logger.info('Ignoring event older than maximum age')
-         return
-    
+def attach_framegrabs(event, grabs):
     aux_data = {
-        'event_id': event['message']['id'],
+        'event_id': event.id,
         'data_source': 'vehicleRealtimeFramegrabberData',
         'data_array': [],
     }
 
-    # Download an image from each framegrabber
-    for (label, url, pattern) in ARGS.grabbers:
-        out_name = pattern.format(ts.strftime("%Y%m%d_%H%M%S%f")[:-3])
-        out_path = os.path.join(ARGS.dest, out_name)
-
-        try:
-            with open(out_path, 'wb') as f:
-                download_url(url, f)
-        except:
-            logger.exception(f'Failed to fetch frame from {label}')
-            continue
-        
+    for label, file in grabs:
         aux_data['data_array'].extend([
             { 'data_name': 'camera_name', 'data_value': label },
 
@@ -85,21 +81,93 @@ async def handle_event(event):
             # The filename provided here will be processed by get_image_url()
             # defined in client_config.js. This currently means that we need to
             # insert a /.
-            { 'data_name': 'filename',    'data_value': f'/{out_name}' },
+            { 'data_name': 'filename',    'data_value': f'/{file}' },
         ])
 
     # Post the new auxiliary data
-    if aux_data['data_array']:
-        logger.info('Associating grabbed frames with event')
-        requests.post(
-            f'{apiServerURL}{eventAuxDataAPIPath}',
-            headers=headers,
-            json=aux_data,
-        )
-    else:
-        logger.warning('Handled event, but could not contact any grabbers')
+    logger.info('Associating grabbed frames with event %s', event.id)
+    requests.post(
+        f'{apiServerURL}{eventAuxDataAPIPath}',
+        headers=headers,
+        json=aux_data,
+    )
 
 
+# Handle an incoming Sealog events by contacting all known framegrabbers and
+# saving the resulting images to the event queue.
+async def handle_event(event):
+    ts = datetime.datetime.strptime(event['message']['ts'],
+                                    '%Y-%m-%dT%H:%M:%S.%fZ')
+    if (datetime.datetime.utcnow() - ts).total_seconds() > ARGS.max_age:
+         logger.info('Ignoring event older than maximum age')
+         return
+
+    # Download an image from each framegrabber to memory
+    # TODO: Do this concurrently!
+    frames = []
+    for (_, url, _) in ARGS.grabbers:
+        try:
+            frames.append(download_url(url))
+        except:
+            logger.exception(f'Failed to fetch frame from {url}')
+            frames.append(None)
+            continue
+
+    if not any(frames):
+        logger.warn('Could not contact any framegrabbers')
+        return
+
+    # Enqueue event for future processing
+    EVENT_QUEUE.put_nowait(Event(
+        id=event['message']['id'],
+        timestamp=ts,
+        frames=frames,
+    ))
+
+
+# This worker pops events with frames off the queue, and submits them to
+# Sealog with the appropriate camera labels attached.
+async def auxdata_worker():
+    while True:
+        event = await EVENT_QUEUE.get()
+        logger.info('Worker popped event %s', event.id)
+
+        # If there hasn't been an imaging control heartbeat since this event
+        # was received, place it back in the queue.
+        if ARGS.imaging_control is not None:
+            dt = None if LAST_HEARTBEAT is None else \
+                 (event.timestamp - LAST_HEARTBEAT).total_seconds()
+            if dt is not None and dt >= 3:
+                logger.info('No heartbeat, doing event %s anyway', event.id)
+            elif dt is None or dt >= 0:
+                logger.info('Re-queuing event %s until heartbeat', event.id)
+                EVENT_QUEUE.put_nowait(event)
+                EVENT_QUEUE.task_done()
+                await asyncio.sleep(0.5)
+                continue
+
+        # Now that we finally know labels for the grabbers, write frames to
+        # disk and attach them to the original Sealog event.
+        grabs = []
+        for grabber, frame in zip(ARGS.grabbers, event.frames):
+            if frame is None:
+                continue
+            label, _, pattern = grabber
+
+            out_name = pattern.replace('{}',
+                event.timestamp.strftime('%Y%m%d_%H%M%S%f')[:-3])
+            out_path = os.path.join(ARGS.dest, out_name)
+            grabs.append((label, out_name))
+
+            with open(out_path, 'wb') as f:
+                f.write(frame)
+
+        attach_framegrabs(event, grabs)
+
+        EVENT_QUEUE.task_done()
+
+
+# Listens for new events coming from Sealog
 async def event_listener():
     async with websockets.connect(wsServerURL) as websocket:
         await websocket.send(json.dumps(HELLO))
@@ -123,6 +191,40 @@ async def event_listener():
                     'An exception occurred while processing a message')
 
 
+# When an imaging control server is in play, this listener updates the list
+# of grabbers whenever the configuration changes.
+async def imaging_control_listener():
+    if ARGS.imaging_control is None:
+        return
+
+    # Perhaps one of the dumbest things I've ever seen: python-socketio just
+    # silently ignores anything other than the server address that you pass
+    # to `url`, so we need to split it up ourselves.
+    #
+    # https://github.com/miguelgrinberg/python-socketio/issues/818
+    u = urllib.parse.urlparse(ARGS.imaging_control)
+
+    sio = socketio.AsyncClient()
+
+    @sio.on('SealogHeartbeat', namespace='/sealog')
+    async def on_heartbeat(hb):
+        global LAST_HEARTBEAT
+        LAST_HEARTBEAT = datetime.datetime.utcnow()
+
+        ARGS.grabbers = []
+        for i, grabber_info in enumerate(hb.get('framegrabbers', [])):
+            ARGS.grabbers.append((
+                f'{grabber_info["camera_name"]} (Framegrabber {i+1})',
+                grabber_info['url'],
+                f'{{}}.{grabber_info["camera_name"]}.framegrab{i:02}.jpg',
+            ))
+
+    await sio.connect(f'{u.scheme}://{u.netloc}',
+                      socketio_path=f'{u.path}/socket.io',
+                      namespaces=['/sealog'])
+    await sio.wait()
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('--dest', required=True)
@@ -130,9 +232,21 @@ if __name__ == '__main__':
                         help='Maximum age of an event that will be annotated')
     parser.add_argument('--timeout', type=float, default=1.0,
                         help='Maximum amount of time to wait for a grabber')
-    parser.add_argument('--grabber', nargs=3, action='append', dest='grabbers',
-                        metavar=('LABEL', 'URL', 'FILENAME_PATTERN'))
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('--grabber', nargs=3, action='append', dest='grabbers',
+                       metavar=('LABEL', 'URL', 'FILENAME_PATTERN'))
+    group.add_argument('--imaging-control', type=str, metavar='URL')
 
     ARGS = parser.parse_args()
 
-    asyncio.run(event_listener())
+    async def start():
+        global EVENT_QUEUE
+        EVENT_QUEUE = asyncio.Queue()
+
+        await asyncio.gather(
+            event_listener(),
+            imaging_control_listener(),
+            auxdata_worker(),
+        )
+
+    asyncio.run(start())
